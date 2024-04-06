b466eeb489947a9a60088058a0649556907e895d
João <joaovitormgv05@gmail.com>
2024-04-06 12:35:31 -0300

    adicionando comentários

--- app/routes/routes.go
+++ app/routes/routes.go
@@ -10,11 +10,15 @@ func SetupRoutes(app *fiber.App, h *handlers.Handlers) {
 	// Rota para criar um novo usuário
 	app.Post("users", h.CreateUserHandler)
 
+	// Rota para encontrar um usuário por ID
 	app.Get("users/:id", h.GetUserHandler)
 
+	// Rota para encontrar todos os usuários
 	app.Get("users", h.GetUsersHandler)
 
+	// Rota para atualizar um usuário por id
 	app.Put("users/:id", h.UpdateUserHandler)
 
+	// Rota para deletar um usuário por id
 	app.Delete("users/:id", h.DeleteUserHandler)
 }
