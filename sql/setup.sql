ea9b841a6dd18f79f42baaf89902720bc50b1a26
João <joaovitormgv05@gmail.com>
2024-04-06 13:38:09 -0300

    corrigindo sql/setup.sql

--- sql/setup.sql
+++ sql/setup.sql
@@ -2,15 +2,15 @@
 -- Ou podem alterar a string de conexão em main.go para uma database sua.
 
 -- Criar database
-CREATE DATABASE IF NOT EXISTS ceos;
+CREATE DATABASE ceos;
 
 -- Conectar com a database
-USE ceos;
+\c ceos;
 
 -- Criar tabela de usuários
 CREATE TABLE IF NOT EXISTS users (
     id SERIAL PRIMARY KEY,
     first_name VARCHAR(255) NOT NULL,
     last_name VARCHAR(255),
-    email VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL
 );
